cc317a8ab2ddfda63b3d1a871165676ded21869e
Andrei Ivanov <05x.andrey@gmail.com>
2023-06-13 17:48:48 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+import tmpnn
+
+setup(
+    name='tmpnn',
+    python_requires='>3.6',
+    version=tmpnn.__version__,
+    packages=find_packages(),
+    author='Andrei Ivanov',
+    author_email='05x.andrey@gmail.com',
+    url='https://github.com/andiva/tmpnn',
+    test_suite='tests',
+)
